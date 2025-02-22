0d2ed175ce90d5ce11835fa75d3d81f183aedbd9
Luke <lukeblazing@gmail.com>
2025-02-22 16:02:48 -0600

    a

--- Client/src/components/dashboard/SideMenuMobile.js
+++ Client/src/components/dashboard/SideMenuMobile.js
@@ -61,7 +61,7 @@ function SideMenuMobile({ open, toggleDrawer }) {
             <Avatar
               sizes="small"
               alt="Luke B"
-              src="/icons/app-icon-512x512.png"
+              src="icons/app-icon-512x512.png"
               sx={{ width: 24, height: 24 }}
             />
             <Typography component="p" variant="h6">
