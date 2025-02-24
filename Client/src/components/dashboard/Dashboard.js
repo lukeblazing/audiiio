5739346269bb0220af48ddf2537bb19dbaf22660
Luke <lukeblazing@gmail.com>
2025-02-23 20:57:09 -0600

    button

--- Client/src/components/dashboard/Dashboard.js
+++ Client/src/components/dashboard/Dashboard.js
@@ -59,8 +59,8 @@ function Dashboard() {
             height: 50, 
             borderRadius: '12px', 
             minWidth: 'auto',
-            transition: 'transform 0.2s ease-in-out',
-            transform: pressedButton === 'remove' ? 'scale(0.4)' : 'scale(1)',
+            transition: 'transform 0.1s ease-in-out',
+            transform: pressedButton === 'remove' ? 'scale(0.7)' : 'scale(1)',
           }}
           disableRipple
           onMouseDown={() => handlePress('remove')}
@@ -78,8 +78,8 @@ function Dashboard() {
             height: 50, 
             borderRadius: '12px', 
             minWidth: 'auto',
-            transition: 'transform 0.2s ease-in-out',
-            transform: pressedButton === 'add' ? 'scale(0.4)' : 'scale(1)',
+            transition: 'transform 0.1s ease-in-out',
+            transform: pressedButton === 'add' ? 'scale(0.7)' : 'scale(1)',
           }}
           disableRipple
           onMouseDown={() => handlePress('add')}
@@ -98,8 +98,8 @@ function Dashboard() {
             border: '2px solid transparent',
             background: 'linear-gradient(white, white) padding-box, linear-gradient(45deg, rgb(255, 0, 140), #FF69B4, rgb(247, 0, 255), rgb(245, 89, 245)) border-box',
             color: 'rgb(255, 0, 140)',
-            transition: 'transform 0.2s ease-in-out',
-            transform: pressedButton === 'ai' ? 'scale(0.4)' : 'scale(1)',
+            transition: 'transform 0.1s ease-in-out',
+            transform: pressedButton === 'ai' ? 'scale(0.7)' : 'scale(1)',
           }}
           disableRipple
           onMouseDown={() => handlePress('ai')}
