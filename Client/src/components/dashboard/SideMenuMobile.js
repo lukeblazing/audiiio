67e51d0db6c392a6ac45a226cc49b8ef5c6bac05
Luke <lukeblazing@gmail.com>
2025-04-17 14:58:49 -0500

    cache issue

--- Client/src/components/dashboard/SideMenuMobile.js
+++ Client/src/components/dashboard/SideMenuMobile.js
@@ -66,7 +66,7 @@ function SideMenuMobile({ open, toggleDrawer }) {
             <Avatar
               sizes="small"
               alt={userData ? userData.name : "User"}
-              src="icons/app-icon-512x512.png"
+              src="icons/app-icon-512x512-v2.png"
               sx={{ width: 24, height: 24 }}
             />
             <Typography component="p" variant="h6">
