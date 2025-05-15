9dbeb9ed700b134e025ba36f40322867c9d99d2b
Luke <lukeblazing@gmail.com>
2025-05-15 12:09:27 -0500

    fix icons

--- Client/src/components/notes/react-sticky-notes/src/components/react-sticky-notes/icons/index.js
+++ Client/src/components/notes/react-sticky-notes/src/components/react-sticky-notes/icons/index.js
@@ -1,12 +1,25 @@
-import { h, getElementStyle } from './../utils';
-const iconsClassName = "material-icons";
-export const add = h('i',{ className: iconsClassName, style: getElementStyle('icon') },'add');
-export const trash = h('i',{ className: iconsClassName, style: getElementStyle('icon') },'delete_outlined');
-export const menu = h('i',{ className: iconsClassName, style: getElementStyle('icon') },'more_horiz');
-export const hide = h('i',{ className: iconsClassName, style: getElementStyle('icon') },'visibility_off');
-export const show = h('i',{ className: iconsClassName, style: getElementStyle('icon') },'minimize');
-export const normalview = h('i',{ className: iconsClassName, style: getElementStyle('icon') },'widgets');
-export const bubbleview = h('i',{ className: iconsClassName, style: getElementStyle('icon') },'grain');
-export const pageview = h('i',{ className: iconsClassName, style: getElementStyle('icon') },'fullscreen');
-export const upload = h('i',{ className: iconsClassName, style: getElementStyle('icon') },'cloud_upload');
-export const fullscreen = h('i',{ className: iconsClassName, style: getElementStyle('icon') },'fullscreen_exit');
+import React from 'react';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import MinimizeIcon from '@mui/icons-material/Minimize';
+import WidgetsIcon from '@mui/icons-material/Widgets';
+import GrainIcon from '@mui/icons-material/Grain';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
+
+import { getElementStyle } from './../utils';
+
+// Replace your icon exports:
+export const add = <AddIcon style={getElementStyle('icon')} />;
+export const trash = <DeleteOutlineIcon style={getElementStyle('icon')} />;
+export const menu = <MoreHorizIcon style={getElementStyle('icon')} />;
+export const hide = <VisibilityOffIcon style={getElementStyle('icon')} />;
+export const show = <MinimizeIcon style={getElementStyle('icon')} />;
+export const normalview = <WidgetsIcon style={getElementStyle('icon')} />;
+export const bubbleview = <GrainIcon style={getElementStyle('icon')} />;
+export const pageview = <FullscreenIcon style={getElementStyle('icon')} />;
+export const upload = <CloudUploadIcon style={getElementStyle('icon')} />;
+export const fullscreen = <FullscreenExitIcon style={getElementStyle('icon')} />;
