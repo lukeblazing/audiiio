3ac477d72e8c1090b0a0c0b28610425f66df2d37
Luke <lukeblazing@gmail.com>
2025-05-28 22:19:12 -0500

    a

--- Client/src/components/calendar/CalendarComponent.js
+++ Client/src/components/calendar/CalendarComponent.js
@@ -496,7 +496,7 @@ const CalendarComponent = ({ }) => {
             sx={{
               width: '90vw',
               height: 'min(max(60vh, 50vw), 120vw)',
-              bgcolor: 'background.paper',
+              bgcolor: '#181c2b',
               borderRadius: 1,
               border: `2px solid ${theme.palette.divider}`
             }}
